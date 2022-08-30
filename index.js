9e215b85b2682f9067dd79b0ac18c274fc3d90de
HariprakashM <hariprakashm20@gmail.com>
2022-08-30 10:29:30 +0530

    second commit

--- index.js
+++ index.js
@@ -5,31 +5,32 @@ var dbconnection = require('./db')
 var productsRoute = require('./routes/productsRoute')
 var userRoute = require('./routes/userRoute')
 var orderRoute = require('./routes/orderRoute')
-const cors=require('cors');
+const cors = require('cors');
 app.use(cors({
-    orgin:"*",
+    orgin: "*",
 }));
 app.use(bodyParser.json());
 const path = require('path');
-app.use('/api/products' , productsRoute)
-app.use('/api/users',userRoute)
-app.use('/api/orders',orderRoute)
+app.use('/api/products', productsRoute)
+app.use('/api/users', userRoute)
+app.use('/api/orders', orderRoute)
 
 
-    if(process.env.NODE_ENV === 'production')
-    {
-        app.use('/' , express.static('client/build'))
+if (process.env.NODE_ENV === 'production') {
+    app.use('/', express.static('client/build'))
 
-        app.get('*' , (req,res)=>{
+    app.get('*', (req, res) => {
 
-            res.sendFile(path.resolve(__dirname , 'client/build/index.html'))
+        res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
 
-        })
+    })
 
 
-    }
+}
 
-   
+app.get("/", (req, res) =>
+    res.send(`Server Running successfully.....!`)
+)
 
 const port = process.env.PORT || 5000;
 
